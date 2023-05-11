dbf7b82fead7240aa4f7b0b869a41d73a5a6d81b
pepe lopez <plopez741@gmail.com>
2023-05-10 17:44:24 -0700

    started on products page

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,49 @@
 import React from 'react'
 
+import { StorefrontImage } from './Images'
+
 
 function Products() {
+
+  const prod = [{
+    productImage: StorefrontImage,
+    name: "test product",
+    price: '$100',
+    description: 'isfdjkngijsnadg',
+  },
+  {
+    productImage: StorefrontImage,
+    name: "test product",
+    price: '$100',
+    description: 'isfdjkngijsnadg',
+  },
+  {
+    productImage: StorefrontImage,
+    name: "test product",
+    price: '$100',
+    description: 'isfdjkngijsnadg',
+  },
+  
+]
+
   return (
-    <div>Products
+    <div>
+      <p>Products</p>
+      <div>
+        {prod.map((item , index) => 
+         {return (
+          <div key={index} >
+          <img src={item.productImage} alt={index}/>
+          <p> {item.name} </p>
+          <p> {item.price} </p>
+          <p> {item.description} </p>
+          <button>#</button>
+        </div>
+         )
+          
+        })}
+        
+      </div>
       
     </div>
   )
